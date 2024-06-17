38742c846d4b30f2e9b5009f026e8971decda869
Chaithanya15 <104422240+Chaithanya15@users.noreply.github.com>
2024-06-17 08:17:25 +0530

    Update apps.py

--- paragrah-api/paragraph/core/apps.py
+++ paragrah-api/paragraph/core/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+# Configuration class for the 'core' application
 
 class CoreConfig(AppConfig):
+     # Set the default auto field type for primary keys
     default_auto_field = 'django.db.models.BigAutoField'
+    # Define the name of the application
     name = 'core'
