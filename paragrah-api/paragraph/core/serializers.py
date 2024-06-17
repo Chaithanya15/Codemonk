12be715b973076e89b4edbda3c78678ce94e9ef7
Chaithanya15 <104422240+Chaithanya15@users.noreply.github.com>
2024-06-17 08:12:39 +0530

    Update serializers.py

--- paragrah-api/paragraph/core/serializers.py
+++ paragrah-api/paragraph/core/serializers.py
@@ -3,13 +3,19 @@
 from rest_framework import serializers
 from .models import Paragraph, WordIndex
 
+# Serializer for the Paragraph model
 class ParagraphSerializer(serializers.ModelSerializer):
     class Meta:
+        # Specify the model to be serialized
         model = Paragraph
+        # Define the fields to be included in the serialization
         fields = ('id', 'content')
 
+# Serializer for the WordIndex model
 class WordIndexSerializer(serializers.ModelSerializer):
     class Meta:
+        # Specify the model to be serialized
         model = WordIndex
+         # Define the fields to be included in the serialization
         fields = ('id', 'word', 'paragraph_id')
 
