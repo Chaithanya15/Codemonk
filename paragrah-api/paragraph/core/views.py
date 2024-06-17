c6cc06df7b4c22620eb77aeca383e1ba2b50e302
Chaithanya15 <104422240+Chaithanya15@users.noreply.github.com>
2024-06-17 08:10:45 +0530

    Update views.py

--- paragrah-api/paragraph/core/views.py
+++ paragrah-api/paragraph/core/views.py
@@ -8,32 +8,48 @@ from .models import Paragraph, WordIndex
 from .serializers import ParagraphSerializer
 from django.db.models import Count
 
+# ViewSet for handling Paragraph-related operations
 class ParagraphViewSet(viewsets.ModelViewSet):
+     # Define the queryset to fetch all Paragraph objects
     queryset = Paragraph.objects.all()
+     # Define the serializer class to be used
     serializer_class = ParagraphSerializer
 
+    # Override the create method to handle custom paragraph creation and word indexing
     def create(self, request, *args, **kwargs):
+         # Retrieve the paragraphs text from the request data
         paragraphs_text = request.data.get('paragraphs', '')
+         # Split the text into individual paragraphs based on double newline characters
         paragraphs_list = paragraphs_text.split('\n\n')
 
+        # Loop through each paragraph text
         for paragraph_text in paragraphs_list:
+            # Create a new Paragraph object with the paragraph text
             paragraph = Paragraph.objects.create(content=paragraph_text)
             words = paragraph_text.lower().split()
             for word in words:
+                # Create a WordIndex entry for each word, linking it to the paragraph
                 WordIndex.objects.create(word=word, paragraph=paragraph)
 
+        # Return a success response
         return Response({'message': 'Paragraphs created successfully'}, status=status.HTTP_201_CREATED)
 
+       # Custom action to search for paragraphs containing a specific word
     @action(detail=False, methods=['get'], url_path='search')
     def search(self, request):
+         # Retrieve the search word from the query parameters and convert it to lowercase
         word = request.query_params.get('word', '').lower()
 
-        # Query to get paragraphs containing the word, ordered by frequency
+        
+         # Query to get paragraphs containing the word, annotated with the count of the word occurrences
+        # Order the paragraphs by the count in descending order and limit to top 10
         paragraphs = (
             Paragraph.objects.filter(words__word=word)
             .annotate(word_count=Count('words'))
             .order_by('-word_count')[:10]
         )
 
+                # Serialize the resulting paragraphs
         serializer = self.get_serializer(paragraphs, many=True)
+         # Return the serialized data in the response
         return Response(serializer.data)
